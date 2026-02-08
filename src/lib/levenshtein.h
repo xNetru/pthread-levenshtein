5b2b60363d600b8f6bd19b672099826d6c7ff965
xNetru <kamilbbank@gmail.com>
2026-02-08 01:07:09 +0100

    feat: add leven status and rearrange parameters order

--- src/lib/levenshtein.h
+++ src/lib/levenshtein.h
@@ -2,15 +2,31 @@
 #define _PTHREAD_LEVENSHTEIN_LEVENSHTEIN_H
 
 #include <stddef.h>
+#include <stdint.h>
+
+#ifndef PL_MAX_WORD_SIZE
+#define PL_MAX_WORD_SIZE 64 * 1024
+#endif
 
 typedef struct leven_data
 {
     const char *first;
     const char *second;
-    const char *dyn_table;
+    const uint32_t *dyn_table;
     size_t first_size;
     size_t second_size;
 } leven_data_t;
+
+typedef enum leven_status
+{
+    // on success
+    success = 0,
+    // on error
+    null_parameters,
+    max_word_size_exceeded,
+    malloc_failure,
+    error,
+} leven_status_t;
 
 typedef struct leven_trans
 {
@@ -27,13 +43,14 @@ typedef struct leven_trans
 typedef struct leven_result
 {
     size_t distance;
-    leven_trans *trans_table;
+    leven_trans_t *trans_table;
 } leven_result_t;
 
-int leven_data_init(leven_data_t *data, const char *first, const char *second);
+leven_status_t leven_data_init(leven_data_t *data, const char *first, const char *second);
 void leven_data_destroy(leven_data_t *data);
 
-int leven_compute(leven_data_t *data, leven_result_t *result);
+leven_status_t leven_compute_dist(leven_result_t *result, leven_data_t *data);
+leven_status_t leven_compute_trans(size_t* result, leven_data_t *data);
 
 void leven_result_free(leven_result_t *result);
 
